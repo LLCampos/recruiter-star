5afc38885423bb88b5d9e9449a5caf1f77b46698
LLCampos <luis.filipe.lcampos@gmail.com>
2021-02-20 10:26:59 +0000

    add fake years per tech elem

--- src/main/scala/com/lcampos/chrome/activetab/Runner.scala
+++ src/main/scala/com/lcampos/chrome/activetab/Runner.scala
@@ -4,7 +4,7 @@ import com.lcampos.chrome.Config
 import com.lcampos.chrome.background.BackgroundAPI
 import com.lcampos.chrome.common.I18NMessages
 import org.scalajs.dom
-import org.scalajs.dom.raw.Element
+import org.scalajs.dom.raw.{Element, NodeList}
 
 import scala.concurrent.ExecutionContext
 
@@ -23,18 +23,41 @@ class Runner(config: ActiveTabConfig, backgroundAPI: BackgroundAPI, messages: I1
 
   private def addYearsPerTechToPage(): Unit = {
     val experienceSectionElem: Element = dom.document.getElementById("experience-section")
-    val yearsPerTech = getYearsPerTech(experienceSectionElem)
-    addBox()
-    log(s"addYearsPerTechToPage was called")
+    val yearsPerTech: Map[String, Int] = getYearsPerTech(experienceSectionElem)
+    addYearsPerTechElem(yearsPerTech)
   }
 
-  private def addBox(): Unit = {
+  private def addYearsPerTechElem(yearsPerTech: Map[String, Int]): Unit = {
     val mainColumnDiv: Element = dom.document.getElementById("main").firstElementChild
     val profileDetail: Element = mainColumnDiv.children.item(1)
     val aboutSection: Element = profileDetail.children.item(2)
-    val aboutSectionCopy: Element = aboutSection.cloneNode(true).asInstanceOf[Element]
-    profileDetail.insertBefore(aboutSectionCopy, profileDetail.firstElementChild)
-    log(s"addBox was called")
+    val yearsPerTechSection: Element = generateYearsPerTechElement(aboutSection)
+    profileDetail.insertBefore(yearsPerTechSection, profileDetail.firstElementChild)
+  }
+
+  private def generateYearsPerTechElement(elementToClone: Element): Element = {
+    val yearsPerTechElem: Element = elementToClone.cloneNode(true).asInstanceOf[Element]
+
+    yearsPerTechElem.querySelector("h2").innerText = "Years Per Tech"
+
+    val yearsPerTechElemP: Element = yearsPerTechElem.querySelector("p")
+
+    val spans: NodeList = yearsPerTechElemP.querySelectorAll("span")
+
+    // Each of these has different classes
+    val spanNormal = spans.item(0).asInstanceOf[Element]
+    val spanLast = spans.item(spans.length - 2).asInstanceOf[Element]
+    val spanEllipsis = spans.item(spans.length - 1).asInstanceOf[Element]
+
+    yearsPerTechElemP.innerHTML = ""
+    spanNormal.innerHTML = "Python -> 1</br>"
+    spanLast.innerHTML = "Java -> 2</br>"
+
+    yearsPerTechElemP.appendChild(spanNormal)
+    yearsPerTechElemP.appendChild(spanLast)
+    yearsPerTechElemP.appendChild(spanEllipsis)
+
+    yearsPerTechElem
   }
 
   private def getYearsPerTech(experienceSectionElem: Element): Map[String, Int] =
