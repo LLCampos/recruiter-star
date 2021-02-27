4ec7649ba79bacc8d8cfb5dcae483cc0e67746be
LLCampos <luis.filipe.lcampos@gmail.com>
2021-02-27 11:17:56 +0000

    simplify template

--- src/main/scala/com/lcampos/duration_per_tech/PageManipulator.scala
+++ src/main/scala/com/lcampos/duration_per_tech/PageManipulator.scala
@@ -38,7 +38,7 @@ object PageManipulator {
           <section class="pv-profile-section pv-about-section artdeco-card p5 mt4 ember-view"><header class="pv-profile-section__card-header">
               <h2 class="pv-profile-section__card-heading">Tech Experience Summary</h2>
               <!----></header>
-              <p id="ember885" class="pv-about__summary-text mt4 t-14 ember-view">
+              <p class="pv-about__summary-text mt4 t-14 ember-view">
               </p>
           </section>
       </div>
