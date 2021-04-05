456c36384623c0406d658a06efe6217965a8c3bf
LLCampos <luis.filipe.lcampos@gmail.com>
2021-04-05 08:45:00 +0100

    redesign clean button

--- src/main/resources/scripts/popup-script.js
+++ src/main/resources/scripts/popup-script.js
@@ -3,7 +3,7 @@ runOnPopup();
 $(function() {
     $("#whichTechnologiesToSee").select2()
 
-    $("#selectedTechnologiesClean").on('click', function (event) {
+    $("#selectedTechnologiesCleanButton").on('click', function (event) {
         $("#whichTechnologiesToSee").val(null).trigger('change')
     })
 });
