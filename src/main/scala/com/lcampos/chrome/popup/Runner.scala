f633c8b7011bf2b07f0b9fef31d1d478467a7601
LLCampos <luis.filipe.lcampos@gmail.com>
2021-04-05 09:19:19 +0100

    on selected tech, sent msg to active tab

--- src/main/scala/com/lcampos/chrome/popup/Runner.scala
+++ src/main/scala/com/lcampos/chrome/popup/Runner.scala
@@ -1,5 +1,6 @@
 package com.lcampos.chrome.popup
 
+import chrome.tabs.bindings.TabQuery
 import com.lcampos.chrome.background.BackgroundAPI
 import com.lcampos.chrome.common.I18NMessages
 import com.lcampos.duration_per_tech.Tech
@@ -41,17 +42,34 @@ class Runner(messages: I18NMessages, backgroundAPI: BackgroundAPI)(implicit ec: 
     ElementUtil.getElementByIdSafeAs[HTMLSelectElement](document, "whichTechnologiesToSee") match {
       case Right(selectElem) =>
         ElementUtil.addOptions(selectElem, Tech.all.map(_.name), selectedTechnologies)
-        selectElem.onchange = (_: Event) => StorageSyncUtil.add(
-          UserConfigKeys.selectedTechnologies,
-          ElementUtil.getAllSelected(selectElem).toJSArray
-        )
+        selectElem.onchange = (_: Event) => onTechSelection(selectElem)
       case Left(err) => println(err)
     }
+
+  private def onTechSelection(selectElem: HTMLSelectElement): Unit = {
+    StorageSyncUtil.add(
+      UserConfigKeys.selectedTechnologies,
+      ElementUtil.getAllSelected(selectElem).toJSArray
+    )
+    sendRefreshExtensionMsgToCurrentTab()
+  }
 
   private def doneButtonHandling(): Unit =
     ElementUtil.getElementByIdSafeAs[HTMLElement](document, "doneButton") match {
       case Right(button) => button.onclick = (_: Event) => window.close()
       case Left(err) => println(err)
+    }
+
+  private def sendRefreshExtensionMsgToCurrentTab(): Unit =
+    sendMsgToCurrentTab("recruiter-star-refresh")
+
+  private def sendMsgToCurrentTab(msg: String): Unit =
+    chrome.tabs.Tabs.query(TabQuery(active = true, currentWindow = true)).onComplete {
+      case Success(tabs) => tabs.head.id.toOption match {
+        case Some(tabId) => chrome.tabs.Tabs.sendMessage(tabId, msg)
+        case None => println("Issue getting current tab id")
+      }
+      case Failure(err) => print(err)
     }
 }
 
