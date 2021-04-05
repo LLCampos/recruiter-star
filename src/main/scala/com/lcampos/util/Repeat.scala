3bd8ff955c9a22052ae3d2cb01bd536e89a2ac48
LLCampos <luis.filipe.lcampos@gmail.com>
2021-04-05 17:21:26 +0100

    fix repeat function

--- src/main/scala/com/lcampos/util/Repeat.scala
+++ src/main/scala/com/lcampos/util/Repeat.scala
@@ -5,10 +5,12 @@ import scala.concurrent.duration.FiniteDuration
 object Repeat {
 
   // Repeat action n times, waiting t between actions.
-  def repeat(n: Int, t: FiniteDuration)(action: => Unit) : Unit =
+  def repeat(n: Int, t: FiniteDuration)(action: => Unit) : Unit = {
+    action
     (1 to n) foreach { _ =>
       scalajs.js.timers.setTimeout(t) {
         action
       }
     }
+  }
 }
